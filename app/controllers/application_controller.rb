0216d46bef8e7153b287169a4fbd5473487e5918
Hidetnk123 <minto1014@icloud.com>
2020-08-21 17:52:18 +0900

    userルーティング設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :katakana_last_name, :katakana_first_name, :birth])
   end
 end
