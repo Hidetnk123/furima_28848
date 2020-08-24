5728f4350fa6117ac8629e49050e98e1be756be3
Hidetnk123 <minto1014@icloud.com>
2020-08-24 14:47:03 +0900

    Emailバリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :email, presence: true,
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true,
                     uniqueness: true,
-                    inclusion: { in: %w(@) }
-  validates :password, presence: true,
+                    format: {with: VALID_EMAIL_REGEX}}
+  validates :password, {presence: true,
                        length: { minimum: 6 },
                        confirmation: true,
-                       format: { with: /\A[a-zA-Z0-9]+\z/, message: "半角英数混合で記入お願いします" }
-  validates :last_name, :first_name, presence: true,
-                                    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角（漢字・ひらがな・カタカナ）で記入お願いします" }
-  validates :katakana_last_name, :katakana_first_name, presence: true,
-                                                      format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で記入お願いします" }
+                       format: { with: /\A[a-zA-Z0-9]+\z/, message: "半角英数混合で記入お願いします" }}
+  validates :last_name, :first_name, {presence: true,
+                                    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角（漢字・ひらがな・カタカナ）で記入お願いします" }}
+  validates :katakana_last_name, :katakana_first_name, {presence: true,
+                                                      format: { with: /\A[ァ-ヶー－]+\z/, message: "全角（カタカナ）で記入お願いします" }}
   validates :birth, presence: true
 end
