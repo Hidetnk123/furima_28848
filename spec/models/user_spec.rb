393ba23ce501eaa6628a84d20f19879c5aa223fd
Hidetnk123 <minto1014@icloud.com>
2020-08-24 19:31:18 +0900

    新規登録うまくいかない時（name、email）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,31 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
       end
     end
+
+    context '新規登録がうまくいかない時' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it "emailが空だと登録できない" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank", "Email is invalid")
+      end
+      it "emailは一意性であるので、重複したら登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "emailは、@がないと登録できない" do
+        @user.email = "test12gamil.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+    end
   end
 end
