3f2715455ff00dc13ecceb16c93110b683165847
Hidetnk123 <minto1014@icloud.com>
2020-08-24 19:29:37 +0900

    新規登録がうまくいく時

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいく時' do
+      it "必須入力フォームが存在すれば登録できる" do
+      expect(@user).to be_valid
+      end
+    end
+  end
 end
