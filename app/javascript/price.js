b992e2e14db4869be85fe7183c4000bc5674a15f
Hidetnk123 <minto1014@icloud.com>
2020-08-27 17:39:26 +0900

    カンマ導入、入力時の動作改善

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,16 +3,16 @@ window.addEventListener('load', function(){
    
     price.addEventListener('keyup', function(){
       let input_price = price.value;
-
+      let tax = document.getElementById("add-tax-price");
+      let sale = document.getElementById("profit");
       let num = 0.1
       let add_tax_price = input_price * num;
       let profit = input_price - add_tax_price;
-        
-      let tax = document.getElementById("add-tax-price");
-      let sale = document.getElementById("profit");
+      let comma_tax = Math.trunc(add_tax_price);
+      let comma_profit = Math.trunc(profit);
 
-      tax.textContent = Math.trunc(add_tax_price);
-      sale.textContent = Math.trunc(profit);
+      tax.textContent = Number(comma_tax).toLocaleString();
+      sale.textContent = Number(comma_profit).toLocaleString();
     })
     
 })
