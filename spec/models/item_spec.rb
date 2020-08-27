e173fd175441d6b3d91032f7590a42add534b6d6
Hidetnk123 <minto1014@icloud.com>
2020-08-27 14:20:30 +0900

    itemのテストコード(imgae, name, content)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('app/assets/images/star.png')
+  end
+
+  describe '商品出品' do
+    context '商品出品がうまくいく時' do
+      it "必須入力フォームが存在すれば出品できる" do
+      expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品がうまくいかない時' do
+      it "imageが存在しないと出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "nameが存在しないと出品できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "contentが存在しないと出品できない" do
+        @item.content = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Content can't be blank")
+      end
+    end
+  end
 end
