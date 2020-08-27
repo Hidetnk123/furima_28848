e22faccddece565ca1053939218bbaaf33433b3f
Hidetnk123 <minto1014@icloud.com>
2020-08-27 14:22:17 +0900

    テストコード(price)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -54,6 +54,26 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank", "Delivery day is not a number")
       end
+      it "priceが存在しないと出品できない" do
+        @item.price  = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "priceは半角数字でないと出品できない" do
+        @item.price  = "aaaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "priceが、¥300以下だと出品できない" do
+        @item.price  = "200"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "priceが、¥9,999,999以上だと出品できない" do
+        @item.price  = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
     end
   end
 end
