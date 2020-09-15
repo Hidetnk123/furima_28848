62820fcff10577c78d609dc89213d0812ecafd4b
Hidetnk123 <minto1014@icloud.com>
2020-09-15 11:21:59 +0900

    見やすく変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_area
   belongs_to_active_hash :delivery_day
 
+  with_options presence: true do
+    validates :name, :content, :image
+    validates :category_id, :item_status_id, :delivery_pay_id, 
+              :delivery_area_id, :delivery_day_id, numericality: { other_than: 1 } 
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
   
-  validates :name, :content, :category_id, :item_status_id, :delivery_pay_id, :delivery_area_id, :delivery_day_id, :image, presence: true
-  validates :category_id, :item_status_id, :delivery_pay_id, :delivery_area_id, :delivery_day_id, numericality: { other_than: 1 } 
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-                    
 end
